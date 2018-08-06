813a376c41642f47274d7cec75e8d0ca18e386bc
ian.housman <ian.housman@gmail.com>
2018-08-06 20:59:27 +0000

    Updated changeDetectionLib.js (added 1 line, deleted 1 line).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -150,7 +150,7 @@ var extractDisturbance = function(lt, distDir, params, mmu) {
 };
 //////////////////////////////////////////////////////////////////////////
 //Function to wrap landtrendr processing
-function landtrendrWrapper(indexName,distDir,run_params,distParams){
+function landtrendrWrapper(processedComposites,indexName,distDir,run_params,distParams){
   //----- RUN LANDTRENDR -----
   var ltCollection = processedComposites.select([indexName]).map(function(img){
     var out = img.multiply(distDir*1000);
