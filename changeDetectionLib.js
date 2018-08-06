73aa208e22257549c6040cb96097a6d831eefe8f
ian.housman <ian.housman@gmail.com>
2018-08-06 21:07:33 +0000

    Updated changeDetectionLib.js (added 2 lines).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -215,6 +215,8 @@ function landtrendrWrapper(processedComposites,indexName,distDir,run_params,dist
   Map.addLayer(distImg.select(['dur']), durVizParms, 'LT-Duration',false);             // add disturbance duration to map
   Map.addLayer(distImg.select(['mag']), magVizParms, 'LT-Magnitude',false);            // add magnitude to map
   Map.addLayer(distImg.select(['yod']), yodVizParms, 'LT-Year of Detection',false);    // add disturbance year of detection to map
+  return [lt,distImg]
+  
 }
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
