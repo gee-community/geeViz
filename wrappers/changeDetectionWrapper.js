04fbead98d0b479e2f724cf993b2e747551a7c4b
iwhousman <iwhousman@gmail.com>
2018-08-10 18:34:00 +0000

    Updated wrappers/changeDetectionWrapper.js (added 1 line, deleted 1 line).

--- wrappers/changeDetectionWrapper.js
+++ wrappers/changeDetectionWrapper.js
@@ -279,7 +279,7 @@ indexDirList.map(function(indexDir){
   var verdetOutputs = dLib.verdetAnnualSlope(tsIndex,startYear,endYear,timebuffer);
   
   //Apply EWMACD
-  var ewmaOutputs = dLib.runEWMACD(lsIndex,startYear,endYear,ewmacdTrainingYears,harmonicCount,annualReducer,!includeSLCOffL7);
+  var ewmaOutputs = dLib.runEWMACD(lsIndex,startYear+timebuffer,endYear-timebuffer,ewmacdTrainingYears,harmonicCount,annualReducer,!includeSLCOffL7);
   var annualEWMA = ewmaOutputs[1].map(function(img){return dLib.multBands(img,1,0.01)});
   
   
