d7a616649ab9d414c3e493e66f60dc55123b993b
iwhousman <iwhousman@gmail.com>
2018-08-10 21:03:17 +0000

    Updated wrappers/changeDetectionWrapper.js (added 1 line, deleted 1 line).

--- wrappers/changeDetectionWrapper.js
+++ wrappers/changeDetectionWrapper.js
@@ -282,7 +282,7 @@ indexDirList.map(function(indexDir){
     c = c.sort('system:time_start');
     
     var bandNames = ee.Image(c.first()).bandNames();
-    bandNames = bandNames.map(function(bn){return ee.String(bn).cat('_slope')})
+    bandNames = bandNames.map(function(bn){return ee.String(bn).cat('Slope')})
     var years = c.toList(10000).map(function(i){i = ee.Image(i);return ee.Date(i.get('system:time_start')).get('year')});
     
     var yearsLeft = years.slice(0,-1);
