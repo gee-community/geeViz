b759577ae8fb5e96e08fc589fa37228053cb7aa2
iwhousman <iwhousman@gmail.com>
2018-08-31 18:54:38 +0000

    Updated wrappers/movingWindowZWrapper.js (added 2 lines, deleted 2 lines).

--- wrappers/movingWindowZWrapper.js
+++ wrappers/movingWindowZWrapper.js
@@ -16,8 +16,8 @@ var studyArea = ee.FeatureCollection('projects/USFS/LCMS-NFS/R1/FNF/FNF_GNP_Merg
 // constraints. This supports wrapping for tropics and southern hemisphere.
 // startJulian: Starting Julian date 
 // endJulian: Ending Julian date
-var startJulian = 200;
-var endJulian = 300; 
+var startJulian = 250;
+var endJulian = 290; 
 
 // 3. Specify start and end years for all analyses
 // More than a 3 year span should be provided for time series methods to work 
