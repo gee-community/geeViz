ec68a63a516114e97c66fca9ee7ae53325b8e22c
iwhousman <iwhousman@gmail.com>
2018-08-31 18:02:47 +0000

    Updated wrappers/harmonicRegressionWrapper.js (added 2 lines, deleted 2 lines).

--- wrappers/harmonicRegressionWrapper.js
+++ wrappers/harmonicRegressionWrapper.js
@@ -27,8 +27,8 @@ var endJulian = 365;
 // More than a 3 year span should be provided for time series methods to work 
 // well. If using Fmask as the cloud/cloud shadow masking method, this does not 
 // matter
-var startYear = 1983;
-var endYear = 1985;
+var startYear = 1984;
+var endYear = 2019;
 
 // 4. Specify an annual buffer to include imagery from the same season 
 // timeframe from the prior and following year. timeBuffer = 1 will result 
