d2d05ef5cd7e578771a783ff32aaf431176c46e7
iwhousman <iwhousman@gmail.com>
2018-09-06 21:14:08 +0000

    Updated wrappers/harmonicRegressionWrapper.js (added 1 line, deleted 1 line).

--- wrappers/harmonicRegressionWrapper.js
+++ wrappers/harmonicRegressionWrapper.js
@@ -187,7 +187,7 @@ var coeffCollection = ee.List.sequence(startYear+timebuffer,endYear-timebuffer,1
   var pa = ee.Image(getImageLib.getPhaseAmplitude(coeffs));
  
   var amplitude = pa.select([0]).multiply(5);
-  var phase = pa.select([1]).unitScale(-Math.PI, Math.PI);
+  var phase = pa.select([1]);
   var val = coeffs.select([0]);
 
   // Turn the HSV data into an RGB image and add it to the map.
