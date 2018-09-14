a6f4d8efb07b2da540bd4c625da6c3669cf42ad2
ian.housman <ian.housman@gmail.com>
2018-09-14 19:43:14 +0000

    Updated wrappers/EWMACDWrapper.js (added 4 lines, deleted 2 lines).

--- wrappers/EWMACDWrapper.js
+++ wrappers/EWMACDWrapper.js
@@ -208,8 +208,10 @@ indexNames.map(function(indexName){
 //Apply EWMACD
 var ewmaOutputs = dLib.runEWMACD(lsIndex,indexName,startYear,endYear,trainingStartYear,trainingEndYear,harmonicCount,annualReducer,!includeSLCOffL7);
 // var annualEWMA = ewmaOutputs[1]//.map(function(img){return dLib.multBands(img,1,0.01)});
-var ewmaRaw = ewmaOutputs[0]
-  Map.addLayer(ewmaRaw,{},indexName + ' ewma',false);
+var ewmaRaw = ewmaOutputs[0];
+var yearRaw = ewmaOutputs[1]
+Map.addLayer(ewmaRaw,{},indexName + ' ewma',false);
+Map.addLayer(yearRaw,{},indexName + ' yearRaw',false);
 //   if(outputCollection === undefined){
 //     outputCollection = annualEWMA
 //   }else{
