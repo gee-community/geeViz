bd455d160d4e98e3ed46a4bfa6931e42ee94ae7d
ian.housman <ian.housman@gmail.com>
2018-09-15 15:22:10 +0000

    Updated changeDetectionLib.js (added 1 line, deleted 1 line).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -552,7 +552,7 @@ var zAndTrendCollection = years.map(function(yr){
     
     //Perform the linear trend analysis
     var linearTrend = getLinearFit(trendImages,indexNames);
-    // print(trendImages.size());
+    print(trendImages.size());
     var linearTrendModel = ee.Image(linearTrend[0]).select(['.*_slope']).multiply(10000);
     
     //Perform the z analysis
