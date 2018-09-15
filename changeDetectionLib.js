e8db2b7b822db9fd12499aeaa5ae30e16f23c85a
ian.housman <ian.housman@gmail.com>
2018-09-15 04:37:34 +0000

    Updated changeDetectionLib.js (added 1 line).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -580,6 +580,7 @@ var zAndTrendCollection = ee.List.sequence(analysisStartYear,endYear,1).map(func
 function thresholdZAndTrend(zAndTrendCollection,zThresh,slopeThresh,startYear,endYear){
   var zCollection = zAndTrendCollection.select('.*_Z');
   var trendCollection = zAndTrendCollection.select('.*_slope');
+  
   var zChange = thresholdChange(zCollection,-zThresh,-1).select('.*_change');
   var trendChange = thresholdChange(trendCollection,-slopeThresh,-1).select('.*_change');
   
