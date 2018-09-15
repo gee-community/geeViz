593b519f72e82d7ef6e86367c9ed26ebb87bbf3c
ian.housman <ian.housman@gmail.com>
2018-09-15 04:42:37 +0000

    Updated changeDetectionLib.js (added 1 line, deleted 1 line).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -606,7 +606,7 @@ function exportZAndTrend(zAndTrendCollection,exportPathRoot,studyArea,scale,crs,
     print(out);
     out.id().evaluate(function(id){
       var outPath = exportPathRoot + '/' + id;
-      getImageLib.exportToAssetWrapper(image,id,outPath,
+      getImageLib.exportToAssetWrapper(out,id,outPath,
         'mean',studyArea,scale,crs,transform);
     });
   });
