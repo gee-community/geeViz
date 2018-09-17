c86c0adeacbca9b8f499770642a3de6d1442adfb
ian.housman <ian.housman@gmail.com>
2018-09-17 02:42:56 +0000

    Updated getImagesLib.js (added 1 line, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -1628,7 +1628,7 @@ function getPhaseAmplitudePeak(coeffs){
       // peakDateBandNames = peakDateBandNames.map(function(bn){return outName.cat(ee.String('_').cat(ee.String(bn)))});
       
       
-      return amplitude//.addBands(phase).addBands(peakDate.rename(peakDateBandNames));
+      return amplitude.addBands(phase)//.addBands(peakDate.rename(peakDateBandNames));
     
     });
     print(phaseAmplitude)
