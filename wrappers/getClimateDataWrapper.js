5950b1c9d2279de886fa10dde8bd4fd73baac262
iwhousman <iwhousman@gmail.com>
2018-09-25 20:09:52 +0000

    Updated wrappers/getClimateDataWrapper.js (added 1 line, deleted 1 line).

--- wrappers/getClimateDataWrapper.js
+++ wrappers/getClimateDataWrapper.js
@@ -107,7 +107,7 @@ function getClimateWrapper(collectionName,studyArea,startYear,endYear,startJulia
           .filterDate(startDate,endDate)
           .filter(ee.Filter.calendarRange(startJulian,endJulian))
   // Create composite time series
-  var ts = getImageLib.compositeTimeSeries(c,startYear,endYear,startJulian,endJulian,timebuffer,weights,compositingReducer);
+  var ts = getImageLib.compositeTimeSeries(c,startYear,endYear,startJulian,endJulian,timebuffer,weights,null,compositingReducer);
   Map.addLayer(ts.select(['prcp']))
   print(ts.size())
   }
