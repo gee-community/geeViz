6181d1847cc25a85973c80a54ea8e6d09e61b219
iwhousman <iwhousman@gmail.com>
2018-09-25 20:48:24 +0000

    Updated getImagesLib.js (added 1 line, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -1210,7 +1210,7 @@ function getModisData(startYear,endYear,startJulian,endJulian,daily,maskWQA,zeni
 function exportCollection(exportPathRoot,outputName,studyArea, crs,transform,scale,
 collection,startYear,endYear,startJulian,endJulian,compositingReducer,timebuffer,exportBands){
   
-  outputName = outputName.replace(/\s+/g,'-');
+  outputName = outputName.replace(/\\+/g,'-');
   collection = collection.select(exportBands);
   ee.List.sequence(startYear+timebuffer,endYear-timebuffer).getInfo()
     .map(function(year){
