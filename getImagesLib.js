3d9bd6e469925fe23d1857faca895c598a8bba3e
iwhousman <iwhousman@gmail.com>
2018-09-25 18:44:40 +0000

    Updated getImagesLib.js (added 1 line, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -657,7 +657,7 @@ function compositeTimeSeries(ls,startYear,endYear,startJulian,endJulian,timebuff
    
     // Compute median or medoid
     var composite;
-    print(typeof(compositingMethod))
+    print(typeof(compositingMethod) === object)
     if (compositingMethod.toLowerCase() === 'median') {
       composite = lsT.median();
     }
