d5faf848f37999ead1800972f0fea79a163078c2
iwhousman <iwhousman@gmail.com>
2018-09-25 18:30:06 +0000

    Updated wrappers/getClimateDataWrapper.js (added 16 lines, deleted 75 lines).

--- wrappers/getClimateDataWrapper.js
+++ wrappers/getClimateDataWrapper.js
@@ -19,8 +19,8 @@ var studyArea = geometry;
 // constraints. This supports wrapping for tropics and southern hemisphere.
 // startJulian: Starting Julian date 
 // endJulian: Ending Julian date
-var startJulian = 190;
-var endJulian = 250; 
+var startJulian = 1;
+var endJulian = 365; 
 
 // 3. Specify start and end years for all analyses
 // More than a 3 year span should be provided for time series methods to work 
@@ -40,7 +40,7 @@ var timebuffer = 1;
 //In order to overweight the center year, you could specify the weights as
 //[1,5,1] which would duplicate the center year 5 times and increase its weight for
 //the compositing method
-var weights = [1,5,1];
+var weights = [1,1,1];
 
 
 
@@ -48,84 +48,25 @@ var weights = [1,5,1];
 // Median tends to be smoother, while medoid retains 
 // single date of observation across all bands
 // If not exporting indices with composites to save space, medoid should be used
-var compositingMethod = 'medoid';
+var compositingMethod = 'median';
 
-// 7. Choose Top of Atmospheric (TOA) or Surface Reflectance (SR) 
+// 7. Choose collection to use
 // Specify TOA or SR
-// Current implementation does not support Fmask for TOA
-var toaOrSR = 'SR';
+// Supports:
+// NASA/ORNL/DAYMET_V3
+// UCSB-CHG/CHIRPS/DAILY (precipitation)
+var collectionName = 'NASA/ORNL/DAYMET_V3';
 
-// 8. Choose whether to include Landat 7
-// Generally only included when data are limited
-var includeSLCOffL7 = false;
 
-//9. Whether to defringe L5
-//Landsat 5 data has fringes on the edges that can introduce anomalies into 
-//the analysis.  This method removes them, but is somewhat computationally expensive
-var defringeL5 = false;
 
-// 10. Choose cloud/cloud shadow masking method
-// Choices are a series of booleans for cloudScore, TDOM, and elements of Fmask
-//Fmask masking options will run fastest since they're precomputed
-//CloudScore runs pretty quickly, but does look at the time series to find areas that 
-//always have a high cloudScore to reduce comission errors- this takes some time
-//and needs a longer time series (>5 years or so)
-//TDOM also looks at the time series and will need a longer time series
-var applyCloudScore = false;
-var applyFmaskCloudMask = true;
 
-var applyTDOM = false;
-var applyFmaskCloudShadowMask = true;
-
-var applyFmaskSnowMask = true;
-
-// 11. Cloud and cloud shadow masking parameters.
-// If cloudScoreTDOM is chosen
-// cloudScoreThresh: If using the cloudScoreTDOMShift method-Threshold for cloud 
-//    masking (lower number masks more clouds.  Between 10 and 30 generally 
-//    works best)
-var cloudScoreThresh = 20;
-
-// Percentile of cloud score to pull from time series to represent a minimum for 
-// the cloud score over time for a given pixel. Reduces comission errors over 
-// cool bright surfaces. Generally between 5 and 10 works well. 0 generally is a
-// bit noisy
-var cloudScorePctl = 10; 
-
-// zScoreThresh: Threshold for cloud shadow masking- lower number masks out 
-//    less. Between -0.8 and -1.2 generally works well
-var zScoreThresh = -1;
-
-// shadowSumThresh: Sum of IR bands to include as shadows within TDOM and the 
-//    shadow shift method (lower number masks out less)
-var shadowSumThresh = 0.35;
-
-// contractPixels: The radius of the number of pixels to contract (negative 
-//    buffer) clouds and cloud shadows by. Intended to eliminate smaller cloud 
-//    patches that are likely errors
-// (1.5 results in a -1 pixel buffer)(0.5 results in a -0 pixel buffer)
-// (1.5 or 2.5 generally is sufficient)
-var contractPixels = 1.5; 
-
-// dilatePixels: The radius of the number of pixels to dilate (buffer) clouds 
-//    and cloud shadows by. Intended to include edges of clouds/cloud shadows 
-//    that are often missed
-// (1.5 results in a 1 pixel buffer)(0.5 results in a 0 pixel buffer)
-// (2.5 or 3.5 generally is sufficient)
-var dilatePixels = 2.5;
-
-// 12. correctIllumination: Choose if you want to correct the illumination using
-// Sun-Canopy-Sensor+C correction. Additionally, choose the scale at which the
-// correction is calculated in meters.
-var correctIllumination = false;
-var correctScale = 250;//Choose a scale to reduce on- 250 generally works well
 
 //13. Export params
 //Whether to export composites
 var exportComposites = true;
 
 //Set up Names for the export
-var outputName = 'Medoid-Landsat';
+var outputName = 'Median-Climate';
 
 //Provide location composites will be exported to
 //This should be an asset folder, or more ideally, an asset imageCollection
@@ -139,7 +80,7 @@ var exportPathRoot = 'users/ianhousman/test';
 var crs = 'EPSG:5070';
 
 //Specify transform if scale is null and snapping to known grid is needed
-var transform = [30,0,-2361915.0,0,-30,3177735.0];
+var transform = [1000,0,-2361915.0,0,-1000,3177735.0];
 
 //Specify scale if transform is null
 var scale = null;
