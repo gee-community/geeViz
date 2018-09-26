9bfa35645312ffa58b6bc49a0ddd1b13b500f583
ian.housman <ian.housman@gmail.com>
2018-09-26 17:23:44 +0000

    Updated getImagesLib.js (added 5 lines, deleted 4 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -1911,11 +1911,12 @@ function getClimateWrapper(collectionName,studyArea,startYear,endYear,startJulia
     
   // Prepare dates
   //Wrap the dates if needed
-  // if (startJulian > endJulian) {
-    // endJulian = endJulian + 365;
-  // }
+  var wrapOffset = 0
+  if (startJulian > endJulian) {
+    wrapOffset = 365;
+  }
   var startDate = ee.Date.fromYMD(startYear,1,1).advance(startJulian-1,'day');
-  var endDate = ee.Date.fromYMD(endYear,1,1).advance(endJulian-1,'day');
+  var endDate = ee.Date.fromYMD(endYear,1,1).advance(endJulian-1+wrapOffset,'day');
   print('Start and end dates:', startDate, endDate);
   print('Julian days are:',startJulian,endJulian);
   //Get climate data
