b7265fccf38f3c6ef371198bec184bff8ba0a495
ian.housman <ian.housman@gmail.com>
2018-10-05 18:27:11 +0000

    Updated getImagesLib.js (added 2 lines, deleted 2 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -99,9 +99,9 @@ function addYearFractionBand(img){
   var d = ee.Date(img.get('system:time_start'));
   var y = d.get('year');
   // d = y.add(d.getFraction('year'));
-  d=d.getFraction('year')
+  d=d.getFraction('year');
   var db = ee.Image.constant(d).rename(['year']).float();
-  db = db//.updateMask(img.select([0]).mask())
+  db = db;//.updateMask(img.select([0]).mask())
   return img.addBands(db);
 }
 function addYearBand(img){
