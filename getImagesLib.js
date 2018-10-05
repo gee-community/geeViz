0647f7a050e4326ca38ed9aba543810c1e784bae
ian.housman <ian.housman@gmail.com>
2018-10-05 19:30:39 +0000

    Updated getImagesLib.js (added 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -470,6 +470,7 @@ function applyCloudScoreAlgorithm(collection,cloudScoreFunction,cloudScoreThresh
       .reduce(ee.Reducer.percentile([cloudScorePctl]));
     Map.addLayer(minCloudScore,{'min':0,'max':30},'minCloudScore',false);
   }else{
+    print('Not computing cloudScore offset');
     var minCloudScore = ee.Image(0).rename(['cloudScore']);
   }
   
