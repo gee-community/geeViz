f98390d5ef6d0e1bde1cec461d09619a71ae4e4e
ian.housman <ian.housman@gmail.com>
2018-10-11 17:42:38 +0000

    Updated wrappers/getCombinedLandsatSentinel2Wrapper.js (added 1 line, deleted 19 lines).

--- wrappers/getCombinedLandsatSentinel2Wrapper.js
+++ wrappers/getCombinedLandsatSentinel2Wrapper.js
@@ -275,28 +275,10 @@ ee.List.sequence(startYear+timebuffer,endYear-timebuffer).getInfo().map(function
                         
                         
                         
-    }))
+    }));
     
 
 
-
-
-
-
-cloudFilter
-60 (string)
-cloudScorePctl
-5 (string)
-zScoreThresh
--1 (string)
-useTDOM
-True
-useCloudProject
-True
-cloudScoreThresh
-20 (string)
-terrain
-True
 
     var outName = exportName+'_y'+startYearT.toString() + '_'+ endYearT.toString() + '_j'+startJulianT.toString() + '_' + endJulianT.toString();
     Map.addLayer(composite,{min:0.05,max:0.5,bands:'swir1,nir,red'},outName,false);
