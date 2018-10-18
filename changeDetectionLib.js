182338c997b405069b4979d9e6c2c4bd2ada2b67
iwhousman <iwhousman@gmail.com>
2018-10-18 16:05:00 +0000

    Updated changeDetectionLib.js (added 13 lines).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -17,6 +17,19 @@ function thresholdChange(changeCollection,changeThresh,changeDir){
   return change;
 }
 
+function thresholdChangeTwoDir(changeCollection,changeThresh,changeDir){
+  if(changeDir === undefined || changeDir === null){changeDir = 1}
+  var bandNames = ee.Image(changeCollection.first()).bandNames();
+  bandNames = bandNames.map(function(bn){return ee.String(bn).cat('_change')});
+  var change = changeCollection.map(function(img){
+    var yr = ee.Date(img.get('system:time_start')).get('year');
+    var changeYr = img.multiply(changeDir).gt(changeThresh);
+    var yrImage = img.where(img.mask(),yr);
+    changeYr = yrImage.updateMask(changeYr).rename(bandNames).int16();
+    return img.mask(ee.Image(1)).addBands(changeYr);
+  });
+  return change;
+}
 function getExistingChangeData(changeThresh,showLayers){
   if(showLayers === undefined || showLayers === null){
     showLayers = true;
