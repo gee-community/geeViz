499b4efb30407bd2e8c55be648c15c47745a425c
ian.housman <ian.housman@gmail.com>
2018-12-05 18:09:12 +0000

    Updated wrappers/getCombinedLandsatSentinel2Wrapper.js (added 27 lines, deleted 24 lines).

--- wrappers/getCombinedLandsatSentinel2Wrapper.js
+++ wrappers/getCombinedLandsatSentinel2Wrapper.js
@@ -192,27 +192,27 @@ Map.addLayer(lsF,getImageLib.vizParamsFalse,'Landsat Cloud/Shadow Masking',false
 Map.addLayer(lsF,getImageLib.vizParamsFalse,'S2 Cloud/Shadow Masking',false);
 
 
-//Apply respective cloudScore functions
-ls = getImageLib.applyCloudScoreAlgorithm(ls,getImageLib.landsatCloudScore,cloudScoreThresh,cloudScorePctl,contractPixels,dilatePixels,performCloudScoreOffset);
-s2s = getImageLib.applyCloudScoreAlgorithm(s2s,getImageLib.sentinel2CloudScore,cloudScoreThresh,cloudScorePctl,contractPixels,dilatePixels,performCloudScoreOffset);
-Map.addLayer(ls.first(),getImageLib.vizParamsFalse,'Landsat Cloud Masking',false);
-Map.addLayer(s2s.first(),getImageLib.vizParamsFalse,'S2 Cloud Masking',false);
+// //Apply respective cloudScore functions
+// ls = getImageLib.applyCloudScoreAlgorithm(ls,getImageLib.landsatCloudScore,cloudScoreThresh,cloudScorePctl,contractPixels,dilatePixels,performCloudScoreOffset);
+// s2s = getImageLib.applyCloudScoreAlgorithm(s2s,getImageLib.sentinel2CloudScore,cloudScoreThresh,cloudScorePctl,contractPixels,dilatePixels,performCloudScoreOffset);
+// Map.addLayer(ls.first(),getImageLib.vizParamsFalse,'Landsat Cloud Masking',false);
+// Map.addLayer(s2s.first(),getImageLib.vizParamsFalse,'S2 Cloud Masking',false);
 
-//Set a property for splitting apart later
-ls = ls.map(function(img){return img.float().set('whichProgram','Landsat')});
-s2s = s2s.map(function(img){return img.float().set('whichProgram','Sentinel2')});
+// //Set a property for splitting apart later
+// ls = ls.map(function(img){return img.float().set('whichProgram','Landsat')});
+// s2s = s2s.map(function(img){return img.float().set('whichProgram','Sentinel2')});
 
-//Merge collections
-var merged = ls.merge(s2s);
+// //Merge collections
+// var merged = ls.merge(s2s);
 
-//Perform TDOM
-merged = getImageLib.simpleTDOM2(merged,zScoreThresh,shadowSumThresh,contractPixels,dilatePixels);
+// //Perform TDOM
+// merged = getImageLib.simpleTDOM2(merged,zScoreThresh,shadowSumThresh,contractPixels,dilatePixels);
 
-//Seperate back out
-ls = merged.filter(ee.Filter.eq('whichProgram','Landsat'));
-s2s = merged.filter(ee.Filter.eq('whichProgram','Sentinel2'));
-Map.addLayer(ls.first(),getImageLib.vizParamsFalse,'Landsat Cloud/Shadow Masking',false);
-Map.addLayer(s2s.first(),getImageLib.vizParamsFalse,'S2 Cloud/Shadow Masking',false);
+// //Seperate back out
+// ls = merged.filter(ee.Filter.eq('whichProgram','Landsat'));
+// s2s = merged.filter(ee.Filter.eq('whichProgram','Sentinel2'));
+// Map.addLayer(ls.first(),getImageLib.vizParamsFalse,'Landsat Cloud/Shadow Masking',false);
+// Map.addLayer(s2s.first(),getImageLib.vizParamsFalse,'S2 Cloud/Shadow Masking',false);
 
 
 
