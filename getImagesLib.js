f68346b13127a2dbea90530b10a00814cd729210
ian.housman <ian.housman@gmail.com>
2018-12-05 17:20:23 +0000

    Updated getImagesLib.js (added 2 lines, deleted 2 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -74,8 +74,8 @@ var bandNames = ['blue','green','red','nir','swir1','swir2']
 var msiOLISlopes = [1.09461,1.0043,1.05241,0.89541,1.00491,1.0002];
 var msiOLIIntercepts = [−0.01071,0.00261,−0.00151,0.00331,0.00651,0.00461];
 
-var msiETMSlopes = [1.10601,0.99091,1.05681,1.0045,1.03611,1.04011]
-var msiETMIntercepts
+var msiETMSlopes = [1.10601,0.99091,1.05681,1.0045,1.03611,1.04011];
+var msiETMIntercepts = [−0.0139,0.00411,−0.0024,−0.0076,0.00411,0.00861];
 
 var oliTMSlopes =[1.03501,1.00921,1.01991,1.14061,1.04351,1.05271]
 
