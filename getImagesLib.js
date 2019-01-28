8821a68a4cc6b2ccc922e1a9c1e6fae99d9a55e2
ian.housman <ian.housman@gmail.com>
2019-01-28 20:31:27 +0000

    Updated getImagesLib.js (added 2 lines, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -1268,7 +1268,8 @@ function modisCloudScore(img) {
   
   // Clouds are reasonably bright in the blue band.
   // score = score.min(rescale(img, 'img.blue', [0.1, 0.3]));
-  Map.addLayer(score,{min:0,max:1},'blue')
+  // Map.addLayer(score,{min:0,max:1},'blue')
+  
   // Clouds are reasonably bright in all visible bands.
   var vizSum = rescale(img, 'img.red + img.green + img.blue', [0.2, 0.8]);
   score = score.min(vizSum);
