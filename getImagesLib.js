56cb1856337ce94a43d0bc9b30cd2b0861eb7fbf
ian.housman <ian.housman@gmail.com>
2019-01-29 20:31:43 +0000

    Updated getImagesLib.js (added 3 lines, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -1514,7 +1514,7 @@ function getModisData(startYear,endYear,startJulian,endJulian,daily,maskWQA,zeni
     var af = ee.Image(a250.first());
     var tf = ee.Image(t250.first());
     var abit1 = af.select(['QC_250m']).bitwiseAnd(Math.pow(2,1));
-    var tbit1 = af.select(['QC_250m']).bitwiseAnd(Math.pow(2,1));
+    var tbit1 = tf.select(['QC_250m']).bitwiseAnd(Math.pow(2,1));
     Map.addLayer(bit1,{min:0,max:2},'bit1')
     Map.addLayer(a250.count(),{min:0,max:16},'aCount')
     Map.addLayer(t250.count(),{min:0,max:16},'tCount')
