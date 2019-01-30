db93377c2060f5f14801257ca17b9233e6b2a187
ian.housman <ian.housman@gmail.com>
2019-01-30 20:12:06 +0000

    Updated getImagesLib.js (added 8 lines, deleted 19 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -1571,25 +1571,14 @@ function getModisData(startYear,endYear,startJulian,endJulian,daily,maskWQA,zeni
               });
               }
               
-              images = images.select(modis500SelectBands,modis500BandNames);
+              images = images.select(modis500SelectBands.cat(dailyViewAngleBandNames),modis500BandNames.cat(dailyViewAngleBandNames));
               return images;
     } 
-    function getAngles(c){
-      var images = ee.ImageCollection(c)
-              .filter(ee.Filter.calendarRange(startYear,endYear,'year'))
-              .filter(ee.Filter.calendarRange(startJulian,endJulian));
-              
-              
-      images = images.select(dailyViewAngleBandNames);
-      return images;
-    }
+    
     var a500 = get500(a500C);
     var t500 = get500(t500C);
     
-    if(addLookAngleBands){
-      var a500Angles = getAngles(a500C);
-      var t500Angles = getAngles(t500C);
-    }
+
     //If thermal collection is wanted, pull it as well
     if(useTempInCloudMask === true){
       var t1000 = ee.ImageCollection(t1000C)
