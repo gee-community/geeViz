f76cbfc07db7ae08cd98fa34850bc712f92650aa
ian.housman <ian.housman@gmail.com>
2019-01-31 22:58:46 +0000

    Updated getImagesLib.js (added 4 lines, deleted 4 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -1294,10 +1294,10 @@ function modisCloudScore(img) {
   //a precomputed mask that may or may not be wanted
   if(useTempInCloudMask === true){
     // Clouds are reasonably cool in temperature.
-    var maskMax = img.select(['temp']).mask().focal_min(5)
-    var tempScore = rescale(img, 'img.temp', [320, 300]);
-    tempScore = ee.Image(1).where(maskMax,tempScore)
-    score = score.min(tempScore);
+    // var maskMax = img.select(['temp']).mask().focal_min(5)
+    // var tempScore = rescale(img, 'img.temp', [320, 300]);
+    // tempScore = ee.Image(1).where(maskMax,tempScore)
+    // score = score.min(tempScore);
     
     score = score.where(img.select(['temp']).mask().not(),1);
   }
