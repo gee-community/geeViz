fe73aa5bdd632020433007d0e1eb42f30cbc0e71
Ian Housman <ihousman@fs.fed.us>
2019-02-22 12:50:50 -0700

    Updated template

---
+++ template/ee/run.js
@@ -0,0 +1,5 @@
+function run(){
+addIDTokenToMap('7a3015af8be667addbde71a3a1a217d1','64691f0003c6db8275a2a1f3a57efd67',{"max": [0.2, 0.95, 0.2], "bands": "swir2,nir,red", "min": 0.1},'Layer 1',true);
+addIDTokenToMap('1b864899e2a0aeeacae8539977830373','b286d0bd263dbce184b6fb29d5c96912',{"max": 20, "min": 0},'minCloudScore',true);
+addIDTokenToMap('21f2c5e6d79b52729ac8519848a7c313','d3083699d423814bdd89c08074435455',{},'Layer 2',true);
+}
